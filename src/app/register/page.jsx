cb6b1dce3fcf1b5a3bee1164d186083860d6b358
Gauri <hassansoft1999@gmail.com>
2024-12-18 00:09:02 +0500

    how

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -42,7 +42,9 @@ const Signup = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow-md">
-        <h2 className="mb-6 text-2xl font-bold text-center">Create Account</h2>
+        <h2 className="mb-6 text-2xl font-bold text-center">
+          Create an Account
+        </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-gray-700">
